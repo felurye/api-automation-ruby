b6519f5154d9acadbcf6267b5b1eb3ceb1548bb1
felurye <s.araujodaniele@gmail.com>
2022-03-23 19:48:14 -0300

    Reorganiza projeto

--- features/services/users.rb
+++ features/services/users.rb
@@ -8,5 +8,13 @@ module Rest
         def get_users
             self.class.get('/usuarios')
         end
+
+        def get_user(id)
+            self.class.get("/usuarios/#{id}")
+        end
+
+        def post_user(user)
+            self.class.post("/usuarios", body: user.to_json)
+        end
     end
 end
